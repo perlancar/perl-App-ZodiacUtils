448aaeb5b35eb54c739f1c7f9d83bfde1f53b610
perlancar (@pc-home) <perlancar@gmail.com>
2015-11-09 14:37:08 +0700

    Initial commit

---
+++ lib/App/ZodiacUtils.pm
@@ -0,0 +1,49 @@
+package App::ZodiacUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+$SPEC{zodiac_of} = {
+    v => 1.1,
+    summary => 'Show zodiac for one or more dates',
+    args => {
+        date => {
+            summary => 'Dates',
+            schema => ['array*', of=>'date*', min_len=>1],
+            'x.perl.coerce_to' => 'DateTime',
+            req => 1,
+            pos => 0,
+        },
+    },
+};
+sub zodiac_of {
+    require DateTime::Event::Zodiac;
+    my %args = @_;
+
+    my $dates = $args{date};
+
+    my $res = [];
+    for my $date (@$dates) {
+        push @$res, DateTime::Event::Zodiac::zodiac_date_name($date);
+    }
+    $res = $res->[0] if @$res == 1;
+    [200, "OK", $res];
+}
+
+1;
+# ABSTRACT: CLI utilities related to zodiac
+
+=head1 DESCRIPTION
+
+This distribution includes the following CLI utilities:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
